fbc231c30c0b1814a5b55e157b186d50bd227309
Stevespear426 <stephen.k.spear@gmail.com>
2012-11-19 16:39:41 -0500

    Longpress navring disable fix
    Derp... with this if the long press is off the behavoir is like stock.
    
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3063,6 +3063,7 @@ public final class Settings {
          * Whether to unlock the screen with the home key.  The value is boolean (1 or 0).
          * @hide
          */
+
         public static final String HOME_UNLOCK_SCREEN = "home_unlock_screen";
 
         /**
