5ac269f659a1d8ae8a3812afc48f2b2e3dd23abd
kufikugel <kufikugel@googlemail.com>
2012-12-17 10:36:17 +0100

    Framework: Add notification rows transparency (2/2)
    Patchset 2+3
    -rebased
    
    Change-Id: Ifdca707989fe836a2a0b46869da808e0750494d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3742,6 +3742,12 @@ public final class Settings {
         */
         public static final String NOTIF_WALLPAPER_ALPHA = "notif_wallpaper_alpha";
 
+        /**
+        * Sets the alpha (transparency) of notifications
+        * @hide
+        */
+        public static final String NOTIF_ALPHA = "notif_alpha";
+
     }
 
     /**
