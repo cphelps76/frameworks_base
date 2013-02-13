41d14a316becfc7660bf1f36ceb417b0bb3cdf11
buckmarble <forrestmarble@gmail.com>
2013-02-12 20:06:29 -0500

    Merge remote-tracking branch 'remotes/aosp/jb-mr1.1-release' into jb-4.2
    Conflicts:
    	core/java/android/os/Trace.java
    	core/java/android/widget/Toast.java
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardMessageArea.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewMediator.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
    	services/java/com/android/server/NotificationManagerService.java
    
    Conflicts:
    
    	core/res/res/drawable-xhdpi/dialog_bottom_holo_dark.9.png
    	core/res/res/drawable-xhdpi/dialog_full_holo_dark.9.png
    	core/res/res/drawable-xhdpi/dialog_middle_holo_dark.9.png
    	core/res/res/drawable-xhdpi/dialog_top_holo_dark.9.png
    	core/res/res/drawable-xhdpi/menu_dropdown_panel_holo_dark.9.png
    	core/res/res/drawable-xhdpi/menu_hardkey_panel_holo_dark.9.png
    	core/res/res/values/config.xml
    	data/sounds/effects/ogg/LowBattery.ogg
    	media/java/android/media/AudioService.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
    
    Conflicts:
    
    	core/java/android/widget/Toast.java
    	core/res/res/drawable-xhdpi/kg_add_widget.png
    	core/res/res/values/config.xml
    	media/java/android/media/AudioService.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -146,7 +146,8 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         public void startObserving() {
             final ContentResolver cr = mContext.getContentResolver();
             cr.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.NEXT_ALARM_FORMATTED), false, this);
+                    Settings.System.getUriFor(Settings.System.NEXT_ALARM_FORMATTED), false, this,
+                    UserHandle.USER_ALL);
         }
     }
 
@@ -198,6 +199,8 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
     private final BugreportObserver mBugreportObserver;
     private final BrightnessObserver mBrightnessObserver;
     private NfcAdapter mNfcAdapter;
+
+    private final boolean mHasMobileData;
 
     private QuickSettingsTileView mUserTile;
     private RefreshCallback mUserCallback;
@@ -328,6 +331,10 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         mBugreportObserver.startObserving();
         mBrightnessObserver = new BrightnessObserver(mHandler);
         mBrightnessObserver.startObserving();
+
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        mHasMobileData = cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
 
         IntentFilter alarmIntentFilter = new IntentFilter();
         alarmIntentFilter.addAction(Intent.ACTION_ALARM_CHANGED);
@@ -460,8 +467,15 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
     }
 
     void onNextAlarmChanged() {
-        mAlarmState.label = Settings.System.getString(mContext.getContentResolver(),
-                Settings.System.NEXT_ALARM_FORMATTED);
+        final String alarmText = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.NEXT_ALARM_FORMATTED,
+                UserHandle.USER_CURRENT);
+        mAlarmState.label = alarmText;
+
+        // When switching users, this is the only clue we're going to get about whether the
+        // alarm is actually set, since we won't get the ACTION_ALARM_CHANGED broadcast
+        mAlarmState.enabled = ! TextUtils.isEmpty(alarmText);
+
         mAlarmCallback.refreshView(mAlarmTile, mAlarmState);
     }
 
@@ -568,6 +582,10 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
             mWifiCallback.refreshView(mWifiTile, mWifiState);
     }
 
+    boolean deviceHasMobileData() {
+        return mHasMobileData;
+    }
+
     // RSSI
     void addRSSITile(QuickSettingsTileView view, RefreshCallback cb) {
         mRSSITile = view;
@@ -585,7 +603,7 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
     public void onMobileDataSignalChanged(
             boolean enabled, int mobileSignalIconId, String signalContentDescription,
             int dataTypeIconId, String dataContentDescription, String enabledDesc) {
-        if (deviceSupportsTelephony()) {
+        if (deviceHasMobileData()) {
             // TODO: If view is in awaiting state, disable
             Resources r = mContext.getResources();
             mRSSIState.signalIconId = enabled && (mobileSignalIconId > 0)
