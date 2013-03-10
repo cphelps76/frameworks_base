333170fc6674a62969a2663ca2c2c2fe9c751d67
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-10 05:58:43 +0100

    SystemUI: Screenshot Toggle
    onClick: Screenshot now(collpase status bar)
    onLongClick: Starts 'timer' and screenshot after timer expires
    
    PS2: Remove rebootmenu calls from PWM and add 500ms delay to allow statusbar to
             fully collapse
    
    PS3,4: Redependcize commits
    PS5: Secure Screenshot
    PS6: Resolved conflicts
    PS8: Respect RC delay setting & fix drawable padding
    PS9: Removed to-do
    PS10: collapseStatusBar() for longclick and added toast
    PS11: reduce toast length to avoid capturing it. also make it translatable
    
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: I2cebb23e530a92901319b7171c3934125568c3ab
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/res/values/strings_toggles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -362,6 +362,41 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     boolean mHideStatusBar;
 
+    private PowerMenuReceiver mPowerMenuReceiver;
+
+    // PowerMenu Tile
+    class PowerMenuReceiver extends BroadcastReceiver {
+        private boolean mIsRegistered = false;
+
+        public PowerMenuReceiver(Context context) {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREENSHOT)) {
+                takeScreenshot();
+            }
+        }
+
+        private void registerSelf() {
+            if (!mIsRegistered) {
+                mIsRegistered = true;
+
+                IntentFilter filter = new IntentFilter();
+                filter.addAction(Intent.ACTION_SCREENSHOT);
+                mContext.registerReceiver(mPowerMenuReceiver, filter);
+            }
+        }
+
+        private void unregisterSelf() {
+            if (mIsRegistered) {
+                mIsRegistered = false;
+                mContext.unregisterReceiver(this);
+            }
+        }
+    }
+
     private static final class PointerLocationInputEventReceiver extends InputEventReceiver {
         private final PointerLocationView mView;
 
@@ -1152,6 +1187,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         + deviceKeyHandlerLib, e);
             }
         }
+        mPowerMenuReceiver = new PowerMenuReceiver(context);
+        mPowerMenuReceiver.registerSelf();
     }
 
     public void setInitialDisplaySize(Display display, int width, int height, int density) {
@@ -3677,7 +3714,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     };
 
     // Assume this is called from the Handler thread.
-    private void takeScreenshot() {
+   public void takeScreenshot() {
         synchronized (mScreenshotLock) {
             if (mScreenshotConnection != null) {
                 return;
