CyanogenMod/android_frameworks_base

Fix pie activation when enabling expanded desktop from QS tile.
Change-Id: I0e8f9b0a723615971a2e14b1d69a71cbabafb26b

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -171,7 +171,7 @@ public class PhoneStatusBarView extends PanelBar {
         mBar.updatePieTriggerMask(PiePosition.LEFT.FLAG
                 | PiePosition.TOP.FLAG
                 | PiePosition.RIGHT.FLAG
-                | PiePosition.TOP.FLAG);
+                | PiePosition.BOTTOM.FLAG);
     }
 
     @Override

cphelps76/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -205,7 +205,7 @@ public class PhoneStatusBarView extends PanelBar {
         mBar.updatePieTriggerMask(Position.LEFT.FLAG
                 | Position.TOP.FLAG
                 | Position.RIGHT.FLAG
-                | Position.TOP.FLAG);
+                | Position.BOTTOM.FLAG);
 
         Settings.System.putInt(mContext.getContentResolver(),
             Settings.System.TOGGLE_NOTIFICATION_SHADE, 0);