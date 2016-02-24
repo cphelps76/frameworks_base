140f30cabb35891a9d2349634f6f6a6d39b13bcb
cphelps76 <cphelps76@gmail.com>
2016-02-24 13:31:14 -0600

    base: rename default launcher package

--- core/java/com/android/internal/app/ResolverActivity.java
+++ core/java/com/android/internal/app/ResolverActivity.java
@@ -240,7 +240,7 @@ public class ResolverActivity extends Activity {
                 } else {
                     PackageManager Pm = this.getPackageManager();
 
-                    Intent mdefaultIntent = Pm.getLaunchIntentForPackage("com.android.launcher3");
+                    Intent mdefaultIntent = Pm.getLaunchIntentForPackage("com.android.cphelps76");
                     mdefaultIntent.addCategory(Intent.CATEGORY_DEFAULT);
                     mdefaultIntent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
                     mdefaultIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
